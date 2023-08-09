ddb79caf6b5ba3dd49979b77885fd60861da3757
Rafael <rafaelmarliere033@gmail.com>
2023-08-09 19:09:30 -0300

    ex002 solved

---
+++ segundo/ex002.c
@@ -0,0 +1,4 @@
+int i=99,j;   // Criação das variáveis.
+int *p;       // Criação do ponteiro.
+p = &i;       // Ponteiro recebe o endereço de memória de i.
+j = *p + 100; // j recebe apontado de p + 100, ou seja, 99 + 100 = 199.
